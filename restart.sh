5544811ab96a37d6e09af32c458bc512cfc3c493
D.B <165270880+BCL-FOSS@users.noreply.github.com>
2025-08-07 22:49:08 +0400

    clean up
    directory clean up for agent testing

--- restart.sh
+++ restart.sh
@@ -1,23 +1,5 @@
-#!/bin/bash
-
-container_setup() {
-
-  # Find the running Caddy container ID
-  CADDY_CONTAINER=$(docker ps --filter "ancestor=caddy" --format "{{.ID}}")
-
-  if [ -z "$CADDY_CONTAINER" ]; then
-      echo "No running Caddy container found."
-      exit 1
-  fi
-
-  echo "Caddy container found: $CADDY_CONTAINER"
-
-  # Format caddyfile
-  docker exec "$CADDY_CONTAINER" sh -c "caddy fmt /etc/caddy/Caddyfile"
-
-  echo "Permissions and ownership updated successfully."
-}
+#!/bin/sh
 
 sudo docker-compose down
 
-sudo docker-compose up --build --remove-orphans
+sudo docker-compose up --build --remove-orphans 
